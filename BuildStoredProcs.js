7c0c063edbf2071ff3db79478dc535205ec35d06
Jesse Gielen <jessegielen@hotmail.com>
2020-10-28 16:43:43 +0100

    Remove upload code

--- BuildStoredProcs.js
+++ BuildStoredProcs.js
@@ -3,7 +3,6 @@ const util = require('util');
 const webpack = require('webpack');
 const fs = require('fs');
 const config = require('./webpack.config');
-const containers = require('./database');
 
 const webpackAsync = util.promisify(webpack);
 const readFile = util.promisify(fs.readFile);
@@ -40,44 +39,4 @@ for (const dir of dirs) {
                 .catch(err => console.log(err))
         );
     }
-
-    // Send scripts to database after compilation complete
-    Promise.all(tasks).then(async () => {
-        // Check if script already exists on db, since upsert is not supported
-        let existingScripts;
-
-        try {
-            existingScripts = await containers[dir].scripts.storedProcedures
-                .readAll()
-                .fetchAll();
-        } catch (err) {
-            console.log(err);
-        }
-
-        for (const script of scripts) {
-            readFile(
-                path.resolve(__dirname, 'output', dir, `${script}.js`),
-                'utf-8'
-            ).then(data => {
-                const body = {
-                    id: script,
-                    // Wrap script in lambda so that azure will accept it
-                    body: `(args) => {\n${data}\n}`,
-                };
-
-                if (existingScripts.resources.some(body => body.id == script)) {
-                    containers[dir].scripts
-                        .storedProcedure(script)
-                        .replace(body)
-                        .then(() => console.log(`${script} replaced on database!`))
-                        .catch(err => console.log(err));
-                } else {
-                    containers[dir].scripts.storedProcedures
-                        .create(body)
-                        .then(() => console.log(`${script} created on database!`))
-                        .catch(err => console.log(err));
-                }
-            });
-        }
-    });
 }
